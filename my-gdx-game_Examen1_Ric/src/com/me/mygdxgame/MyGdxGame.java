2f728bf57dd59b24875731c1829747964af45abc
Alumno <alumno@localhost.localdomain>
2014-05-20 08:40:05 -0600

    parte 2

--- my-gdx-game_Examen1_Ric/src/com/me/mygdxgame/MyGdxGame.java
+++ my-gdx-game_Examen1_Ric/src/com/me/mygdxgame/MyGdxGame.java
@@ -13,71 +13,68 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class MyGdxGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
+	/*private OrthographicCamera camera;
+	private SpriteBatch batch;*/
 	private Texture texture;
-	private Sprite sprite;
-	private Texture texture_circulo;
-	private Sprite sprite_circulo;
-	private Texture texture_rectangulo;
-	private Sprite sprite_rectangulo;
-	private Texture texture_estrella;
-	private Sprite sprite_estrella;
+	//Aqui declaramos o inicializamos las imagenes que utilizaremos//
 	Stage stage;
-	Image image;
+	Image fondo;
+	Image circulo;
+	Image rectangulo;
+	Image estrella;
+	Image triangulo;
+	Image pentagono;
+
 	
+	boolean izquierda=false;
+	boolean derecha=false;
+
+
 	@Override
 	public void create() {		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+
+		texture = new Texture(Gdx.files.internal("data/circulo.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		texture_circulo = new Texture(Gdx.files.internal("data/circulo.png"));
-		texture_rectangulo = new Texture(Gdx.files.internal("data/rectangulo.png"));
-		texture_estrella = new Texture(Gdx.files.internal("data/estrella.png"));
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite_circulo= new Sprite(texture_circulo,512,512); 
-		sprite_circulo.setPosition(-0.4f,-0.2f); 
-		sprite_circulo.setSize(0.4f,0.4f);
-		sprite_circulo.setX(-0.1f);
-		
-		sprite_rectangulo= new Sprite(texture_rectangulo,512,512); 
-		sprite_rectangulo.setPosition(-0.1f,-0.2f); 
-		sprite_rectangulo.setSize(0.4f,0.4f);
-		sprite_rectangulo.setRotation(0);
-		
-		sprite_estrella= new Sprite(texture_estrella,512,512); 
-		sprite_estrella.setPosition(-0.4f,-0.2f); 
-		sprite_estrella.setSize(0.4f,0.4f);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+
+		TextureRegion region = new TextureRegion(texture, 0, 0, 128, 128);
+
+		//Aqui colocamos la direccion donde tenemos la imagenes en el sistema//
 		stage = new Stage();
-		image = new Image(region);
+		circulo = new Image(region);
+		stage = new Stage();
+		rectangulo = new Image(new Texture(Gdx.files.internal("data/rectangulo.png")));
+		estrella = new Image(new Texture(Gdx.files.internal("data/estrella.png")));
+		triangulo = new Image(new Texture(Gdx.files.internal("data/triangulo.png")));
+		pentagono = new Image(new Texture(Gdx.files.internal("data/pentagono.png")));
+		fondo = new Image(new Texture(Gdx.files.internal("data/fondo.png")));
+
+
+		//Aqui puse la posicion de las figuras//
+		fondo.setX(0);
+		fondo.setY(-100);
+		circulo.setX(100);
+		circulo.setY(100);
+		rectangulo.setX(400);
+		rectangulo.setY(100);
+		estrella.setX(200);
+		estrella.setY(100);
+		pentagono.setX(100);
+		pentagono.setY(50);
 		
-		sprite_circulo.setX(sprite_circulo.getX()-0.1f);
-	
-		//image.setRotation(45);
-		//if(sprite_rectangulo.getX()>700)//
-			//sprite_rectangulo.setX(-500);//
-		//image.setX(50);//
 		
-		stage.addActor(image);
-		
-		
+		stage.addActor(fondo);
+		stage.addActor(circulo);
+		stage.addActor(rectangulo);
+		stage.addActor(estrella);
+		stage.addActor(triangulo);
+		stage.addActor(pentagono);
+
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
 		texture.dispose();
 	}
 
@@ -85,19 +82,79 @@ public class MyGdxGame implements ApplicationListener {
 	public void render() {		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		rectangulo.setRotation(rectangulo.getRotation()-1);
+		circulo.setX(circulo.getX()+2);
+		pentagono.setY(pentagono.getY()-0.4f);
+		pentagono.setX(pentagono.getX()+0.5f);
+		pentagono.setY(pentagono.getY()+0.1f);
+		pentagono.setX(pentagono.getX()-0.5f);
+		estrella.setY(estrella.getY()+3);
+		estrella.setX(estrella.getX()-1);
+		estrella.setScale(estrella.getScaleX()-0.1f,estrella.getScaleY()+0.01f);
+
+		//Aqui en esta funcion muevo el triangulo en ambas direcciones varias veces//
+		if (triangulo.getX()>300)
+		{
+			izquierda=true;
+		}
+		
+		if(triangulo.getX()<10)
+		{
+			izquierda = false;
+		}
+
+		if (izquierda==true)
+		{
+			triangulo.setX(triangulo.getX()-10);
+		}
+		else
+		{
+			triangulo.setX(triangulo.getX()+10);
+		}
+
+		
+		/*//PENTAGONO
+		if (pentagono.getY()>300)
+		{
+			izquierda=true;
+		}
+		else
+		{
+			pentagono.setX(pentagono.getX()-1f);
+		}
+		
+		if (pentagono.getY()>300)
+		{
+			izquierda=true;
+		}
+		else
+		{
+			pentagono.setY(pentagono.getY()-1f);
+		}
 		
 		
+		if (pentagono.getX()>300)
+		{
+			izquierda=true;
+		}
+
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite_circulo.draw(batch);
-		sprite_circulo.setX(-0.1f);
-		sprite_rectangulo.draw(batch);
-		sprite_rectangulo.setRotation(0);
-		//sprite.draw(batch);//
-		batch.end();
+		if(pentagono.getY()<1)
+		{
+			izquierda = false;
+		}
+
+		if (izquierda==true)
+		{
+			pentagono.setX(pentagono.getX()-2);
+		}
+		else
+		{
+			pentagono.setX(pentagono.getX()+5);
+		}*/
 		
-		sprite_circulo.setX(sprite_circulo.getX()+0.01f);
+		//Con esto imprimimos las imagenes por ejemplo yo las guarde todas en un solo stage//
+		stage.draw();
 	}
 
 	@Override
